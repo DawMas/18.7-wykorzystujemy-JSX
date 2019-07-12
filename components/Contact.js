1481571c201359cf94ff9e297e4b174264738811
Dawid Maslankiewicz <maslankiewiczd@gmail.com>
2019-07-13 01:06:39 +0200

    Change contact and contactsForm to JSX

--- components/Contact.js
+++ components/Contact.js
@@ -5,14 +5,18 @@ var Contact = React.createClass({
 
   render: function () {
     return (
-      React.createElement('div', { className: 'contactItem' },
-        React.createElement('img', { className: 'contactImage', src: 'https://static.thenounproject.com/png/45219-200.png' }),
-        React.createElement('div', { className: 'contactInfo' },
-          React.createElement('p', { className: 'contactLabel' }, 'Imię: ' + this.props.item.firstName),
-          React.createElement('p', { className: 'contactLabel' }, 'Nazwisko: ' + this.props.item.lastName),
-          React.createElement('a', { className: 'contactEmail', href: 'mailto:' + this.props.item.email }, this.props.item.email),
-        )
-      )
+      <div className={'contactItem'}>
+        <img className={'contactImage'} src={'https://static.thenounproject.com/png/45219-200.png'} />
+        <div className={'contactInfo'}>
+          <p className={'contactLabel'}>Imię: {this.props.item.firstName}
+          </p>
+          <p className={'contackLabel'}>Nazwisko: {this.props.item.lastName}
+          </p>
+          <a className={'contactEmail'} href={'mailto:' + this.props.item.email}>
+            {this.props.item.email}
+          </a>
+        </div>
+      </div>
     )
   },
 });
